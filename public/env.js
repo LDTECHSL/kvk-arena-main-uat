8372be9474ebd401095ab485e7864ff17b1b91c5
Dasun Shyaminda <dasunshyaminda0518@gmail.com>
2026-08-23 21:41:49 +0530

    fsfvdv

---
+++ public/env.js
@@ -0,0 +1,13 @@
+window.env = {
+  API_URL: "https://localhost:7007/api/",
+  // API_URL: "https://klc.runasp.net/api/",
+  // API_URL: "https://api-kvkarena.premiumasp.net/api/",
+  BASE_URL: "https://kvkarena.lk/",
+  OFFER_END_DATE: "2026-08-20",
+  MEMBERSHIP_PRICE: "100,000",
+  GYM_RATE: "10",
+  BADMINTON_RATE: "10",
+  CARWASH_RATE: "10",
+  GAMING_RATE: "10",
+  CAFE_RATE: "10",
+};
