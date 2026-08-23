02f8b4710fb2894d7a16ecc76cac3a6d52d37ca9
Dasun Shyaminda <dasunshyaminda0518@gmail.com>
2026-08-23 22:29:41 +0530

    mmm

--- public/env.js
+++ public/env.js
@@ -1,6 +1,6 @@
 window.env = {
-  API_URL: "https://localhost:7007/api/",
-  // API_URL: "https://klc.runasp.net/api/",
+  // API_URL: "https://localhost:7007/api/",
+  API_URL: "https://klc.runasp.net/api/",
   // API_URL: "https://api-kvkarena.premiumasp.net/api/",
   BASE_URL: "https://kvkarena.lk/",
   OFFER_END_DATE: "2026-08-20",
